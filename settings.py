ab05d06b939a26dfa426f7b3287687954046bfa0
lillisgary <lillisgary@gmail.com>
2015-07-21 17:57:55 +0200

    pip freeze requirements and altered settings.py

--- settings.py
+++ settings.py
@@ -388,23 +388,23 @@ DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
 
-# FABRIC = {
-#     "SSH_USER": "", # SSH username
-#     "SSH_PASS":  "", # SSH password (consider key-based authentication)
-#     "SSH_KEY_PATH":  "", # Local path to SSH key file, for key-based auth
-#     "HOSTS": [], # List of hosts to deploy to
-#     "VIRTUALENV_HOME":  "", # Absolute remote path for virtualenvs
-#     "PROJECT_NAME": "", # Unique identifier for project
-#     "REQUIREMENTS_PATH": "", # Path to pip requirements, relative to project
-#     "GUNICORN_PORT": 8000, # Port gunicorn will listen on
-#     "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
-#     "LIVE_HOSTNAME": "www.example.com", # Host for public site.
-#     "REPO_URL": "", # Git or Mercurial remote repo URL for the project
-#     "DB_PASS": "", # Live database password
-#     "ADMIN_PASS": "", # Live admin user password
-#     "SECRET_KEY": SECRET_KEY,
-#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
-# }
+FABRIC = {
+    "SSH_USER": "", # SSH username
+    "SSH_PASS":  "", # SSH password (consider key-based authentication)
+    "SSH_KEY_PATH":  "", # Local path to SSH key file, for key-based auth
+    "HOSTS": [], # List of hosts to deploy to
+    "VIRTUALENV_HOME":  "", # Absolute remote path for virtualenvs
+    "PROJECT_NAME": "", # Unique identifier for project
+    "REQUIREMENTS_PATH": "", # Path to pip requirements, relative to project
+    "GUNICORN_PORT": 8000, # Port gunicorn will listen on
+    "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
+    "LIVE_HOSTNAME": "www.example.com", # Host for public site.
+    "REPO_URL": "", # Git or Mercurial remote repo URL for the project
+    "DB_PASS": "default", # Live database password
+    "ADMIN_PASS": "default", # Live admin user password
+    "SECRET_KEY": "default",
+    "NEVERCACHE_KEY": "default",
+}
 
 
 ##################
