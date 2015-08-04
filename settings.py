a27a11d1338e0e0d5af31335d1156a24439e2298
lillisgary <lillisgary@gmail.com>
2015-08-04 20:24:22 +0200

    added user accounts application in settings.py

--- settings.py
+++ settings.py
@@ -318,7 +318,7 @@ INSTALLED_APPS = (
     "mezzanine.pages",
     "mezzanine.galleries",
     "mezzanine.twitter",
-    #"mezzanine.accounts",
+    "mezzanine.accounts",
     #"mezzanine.mobile",
 )
 
